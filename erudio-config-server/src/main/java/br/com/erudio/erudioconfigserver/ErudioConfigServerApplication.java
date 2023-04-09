3ad7eeb8f51d4880adc071d2903f7042d140b62b
Rafael Santos <rafa.05125@gmail.com>
2023-04-08 23:51:17 -0300

    Config server created

---
+++ erudio-config-server/src/main/java/br/com/erudio/erudioconfigserver/ErudioConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.erudioconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErudioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErudioConfigServerApplication.class, args);
+	}
+
+}
